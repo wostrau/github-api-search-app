4b1b075eaab9336236280e61e9635664b15f8aba
wostrau <a.ostrovtsov@gmail.com>
2023-04-06 22:56:46 +0200

    #23 from Samurai Way 2.0 playlist / stopped at 01:15:00 / topic: useEffect using Github API

--- src/components/Github.tsx
+++ src/components/Github.tsx
@@ -5,21 +5,34 @@ import axios from 'axios'
 
 type SearchUserType = { login: string, id: number }
 type SearchResultType = { items: SearchUserType[] }
+type UserResultType = {
+    login: string
+    id: number
+    avatar_url: string
+    followers: number
+}
 
 export const Github = () => {
-    const [selectedUser, setSelectedUser] = useState<string | null>(null)
+    const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null)
+    const [userDetails, setUserDetails] = useState<UserResultType | null>(null)
     const [users, setUsers] = useState<SearchUserType[]>([])
     const [tempSearch, setTempSearch] = useState<string>('')
+    const [finalSearch, setFinalSearch] = useState<string>('')
 
     useEffect(() => {
-        if (selectedUser) document.title = selectedUser
+        if (selectedUser) document.title = selectedUser.login
     }, [selectedUser])
-
     useEffect(() => {
         axios
-            .get<SearchResultType>(`https://api.github.com/search/users?q=${tempSearch}`)
+            .get<SearchResultType>(`https://api.github.com/search/users?q=${finalSearch}`)
             .then(res => setUsers(res.data.items))
-    }, [])
+    }, [finalSearch])
+    useEffect(() => {
+        if (!selectedUser) return
+        axios
+            .get<UserResultType>(`https://api.github.com/users/${selectedUser.login}`)
+            .then(res => setUserDetails(res.data))
+    }, [selectedUser])
 
     return (
         <div className={s.container}>
@@ -30,11 +43,7 @@ export const Github = () => {
                         onChange={event => setTempSearch(event.currentTarget.value)}
                     />
                     <button
-                        onClick={() => {
-                            axios
-                                .get<SearchResultType>(`https://api.github.com/search/users?q=${tempSearch}`)
-                                .then(res => setUsers(res.data.items))
-                        }}
+                        onClick={() => setFinalSearch(tempSearch)}
                     >Find
                     </button>
                 </div>
@@ -43,16 +52,25 @@ export const Github = () => {
                         return (
                             <li
                                 key={u.id}
-                                className={selectedUser === u.login ? s.selected : ''}
-                                onClick={() => setSelectedUser(u.login)}
+                                className={selectedUser?.login === u.login ? s.selected : ''}
+                                onClick={() => setSelectedUser(u)}
                             >{u.login}</li>
                         )
                     })}
                 </ul>
             </div>
             <div>
-                <h2>Username</h2>
-                <div>Details</div>
+                {!!userDetails && <>
+                    <h2>{userDetails.login}</h2>
+                    <img
+                        src={userDetails.avatar_url}
+                        alt='userPhoto'
+                        style={{width: '200px'}}
+                    />
+                    <div>userId: {userDetails.id}</div>
+                    <div>followers: {userDetails.followers}</div>
+                </>}
+
             </div>
         </div>
     )
