8ecc0627b219cda00a3a6b985a0367c9c1f4c3e9
wostrau <a.ostrovtsov@gmail.com>
2023-04-07 14:08:38 +0200

    #23 from Samurai Way 2.0 playlist / stopped at 01:51:00 / topic: useEffect using Github API

--- src/components/Github.tsx
+++ src/components/Github.tsx
@@ -3,6 +3,58 @@ import s from './Github.module.css'
 import axios from 'axios'
 
 
+type SearchPropsType = { value: string, onSubmit: (fixedValue: string) => void }
+export const Search: React.FC<SearchPropsType> = ({value, onSubmit}) => {
+    const [tempSearch, setTempSearch] = useState<string>('')
+
+    useEffect(() => setTempSearch(value), [value])
+
+    return (
+        <div>
+            <input
+                placeholder={'search'}
+                value={tempSearch}
+                onChange={event => setTempSearch(event.currentTarget.value)}
+            />
+            <button
+                onClick={() => onSubmit(tempSearch)}
+            >Find
+            </button>
+        </div>
+    )
+}
+
+
+type UsersListPropsType = {
+    term: string
+    selectedUser: SearchUserType | null
+    onUserSelect: (user: SearchUserType) => void
+}
+export const UsersList: React.FC<UsersListPropsType> = ({term, selectedUser, onUserSelect}) => {
+    const [users, setUsers] = useState<SearchUserType[]>([])
+
+    useEffect(() => {
+        axios
+            .get<SearchResultType>(`https://api.github.com/search/users?q=${term}`)
+            .then(res => setUsers(res.data.items))
+    }, [term])
+
+    return (
+        <ul>
+            {users.map(u => {
+                return (
+                    <li
+                        key={u.id}
+                        className={selectedUser?.login === u.login ? s.selected : ''}
+                        onClick={() => onUserSelect(u)}
+                    >{u.login}</li>
+                )
+            })}
+        </ul>
+    )
+}
+
+
 type SearchUserType = { login: string, id: number }
 type SearchResultType = { items: SearchUserType[] }
 type UserResultType = {
@@ -15,18 +67,13 @@ type UserResultType = {
 export const Github = () => {
     const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null)
     const [userDetails, setUserDetails] = useState<UserResultType | null>(null)
-    const [users, setUsers] = useState<SearchUserType[]>([])
-    const [tempSearch, setTempSearch] = useState<string>('')
-    const [finalSearch, setFinalSearch] = useState<string>('')
+    let initialValue = 'wostrau'
+    const [finalSearch, setFinalSearch] = useState<string>(initialValue)
 
     useEffect(() => {
         if (selectedUser) document.title = selectedUser.login
     }, [selectedUser])
-    useEffect(() => {
-        axios
-            .get<SearchResultType>(`https://api.github.com/search/users?q=${finalSearch}`)
-            .then(res => setUsers(res.data.items))
-    }, [finalSearch])
+
     useEffect(() => {
         if (!selectedUser) return
         axios
@@ -37,34 +84,30 @@ export const Github = () => {
     return (
         <div className={s.container}>
             <div>
-                <div>
-                    <input
-                        placeholder={'search'}
-                        onChange={event => setTempSearch(event.currentTarget.value)}
-                    />
-                    <button
-                        onClick={() => setFinalSearch(tempSearch)}
-                    >Find
-                    </button>
-                </div>
-                <ul>
-                    {users.map(u => {
-                        return (
-                            <li
-                                key={u.id}
-                                className={selectedUser?.login === u.login ? s.selected : ''}
-                                onClick={() => setSelectedUser(u)}
-                            >{u.login}</li>
-                        )
-                    })}
-                </ul>
+                <Search
+                    value={finalSearch}
+                    onSubmit={(fixedValue: string) => setFinalSearch(fixedValue)}
+                />
+                <button
+                    onClick={() => {
+                        setFinalSearch(initialValue)
+                        setSelectedUser({login: initialValue, id: 29176361})
+                    }}
+                >Reset
+                </button>
+                <UsersList
+                    term={finalSearch}
+                    selectedUser={selectedUser}
+                    onUserSelect={setSelectedUser}
+                />
+
             </div>
             <div>
                 {!!userDetails && <>
                     <h2>{userDetails.login}</h2>
                     <img
                         src={userDetails.avatar_url}
-                        alt='userPhoto'
+                        alt="userPhoto"
                         style={{width: '200px'}}
                     />
                     <div>userId: {userDetails.id}</div>
